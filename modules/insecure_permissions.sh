368d3ab88e99cbc84148661b464090bc7af211a0
Atul-Pal <kingironcobra3000@gmail.com>
2025-12-07 22:15:53 +0530

    Change variable name from REPO to FILES for better understanding

--- modules/insecure_permissions.sh
+++ modules/insecure_permissions.sh
@@ -3,7 +3,7 @@
 
 
 OUTPUT="../output/insecure_permissions.txt"
-REPO="../tests"
+FILES="../tests"
 
 
 
@@ -19,14 +19,14 @@ echo "" >> "$OUTPUT"
 # Detect files with 777 permissions
 
 echo "---Files with 777 permissions---:" >> "$OUTPUT"
-find "$REPO" -type f -perm 777 2>/dev/null >> "$OUTPUT"
+find "$FILES" -type f -perm 777 2>/dev/null >> "$OUTPUT"
 echo "" >> "$OUTPUT"
 
 
 
 # Detect directories with 777 permissions
 echo "---Directories with 777 permissions---:" >> "$OUTPUT"
-find "$REPO" -type d -perm 777 2>/dev/null  >> "$OUTPUT"
+find "$FILES" -type d -perm 777 2>/dev/null  >> "$OUTPUT"
 echo "" >> "$OUTPUT"
 
 
